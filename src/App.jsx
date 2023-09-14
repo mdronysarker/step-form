48dd3b6bc6a6cd2ac6395847c284206ad5a091db
Md Rony <mdronyrsk01724@gmail.com>
2023-09-14 17:47:54 +0600

    step form desing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const App = () => {
-  return <div>hiii</div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">Hello</p>
+      <div className="buttons">
+        <button>Previous</button>
+        <button>Next</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
